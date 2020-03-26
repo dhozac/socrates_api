566e200e06820aa8331b16aea3082e6796c2dbdb
Daniel Hokka Zakrisson <daniel@hozac.com>
2020-03-26 15:32:46 +0100

    Version 1.3.5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import os
 import re
 
 setup(name='socrates_api',
-      version='1.3.4',
+      version='1.3.5',
       license='Apache Software License',
       description='Source of Truth for hardware, virtual machines, and networks',
       author='Klarna Bank AB',
